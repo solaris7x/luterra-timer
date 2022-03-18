65ca324acd2c6794aa4f3b562105dc5d6865ba94
solaris7x <question211199@gmail.com>
2022-03-18 23:16:12 +0530

    Added herobeam font Default font Comic Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        herobeam: ["herobeam", "Comic Sans MS", "Comic Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
