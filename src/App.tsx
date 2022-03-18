641d62161cb8876f97a49c8938b628abb9b6e56e
solaris7x <question211199@gmail.com>
2022-03-18 21:08:03 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import NavBar from "./layout/NavBar";
+
+const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1 className="text-3xl font-bold text-pink-400 underline">
+        Hello Luterra!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/layout/NavBar.tsx
@@ -0,0 +1,8 @@
+const NavBar = () => {
+  return (
+    <div className="w-full bg-gray-700 p-2 text-2xl text-white text-center">
+      Luterra Timer
+    </div>
+  );
+};
+export default NavBar;
